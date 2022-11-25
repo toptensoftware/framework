ceb8dad1267aa779ec1fb87eebb0a3c0eedd72c1
Brad Robinson <contact@toptensoftware.com>
2022-11-25 17:45:08 +1100

    Tooltips working

--- framework.js
+++ framework.js
@@ -7,3 +7,44 @@ document.body.addEventListener('click', function(event) {
     }
 });
 
+let _currentPopper;
+let _currentTooltip;
+
+document.body.addEventListener('mouseenter', function(event) {
+
+    // Find tooltip data
+    let tooltipData = event.target.getAttribute('data-sv-tooltip');
+    if (!tooltipData)
+        return;
+
+    // Create tooltip
+    let _currentTooltip = document.createElement("div");
+    _currentTooltip.classList.add('tooltip');
+    _currentTooltip.innerText = tooltipData;
+    document.body.appendChild(_currentTooltip);
+
+    // Create popper
+    _currentPopper = Popper.createPopper(event.target, _currentTooltip, {
+        placement: event.target.getAttribute('data-popper-placement') || 'bottom',
+        modifiers: [
+            { name: 'offset', options: { offset: [0, 4] }},
+        ]
+    });
+
+    _currentPopper.update();
+
+    event.target.addEventListener('mouseleave', function(event) {
+
+        // Remove tooltip
+        if (_currentTooltip)
+            _currentTooltip.remove();
+
+        // Remove popper
+        if (_currentPopper)
+        {
+            _currentPopper.destroy();
+            _currentPopper = null;
+        }
+    });
+    
+}, true);
